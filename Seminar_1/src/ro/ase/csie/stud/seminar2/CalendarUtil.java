ef47928def7756fcefcfc78ae046aeb288388d2c
stoia <stoia@LAPTOP-4DK1TJ8U>
2021-03-04 13:58:52 +0200

    Seminar2

---
+++ Seminar_1/src/ro/ase/csie/stud/seminar2/CalendarUtil.java
@@ -0,0 +1,23 @@
+package ro.ase.csie.stud.seminar2;
+
+public class CalendarUtil {
+	
+	public String weekDay(int day)
+	{
+		if(day==1)
+			return "Sunday";
+		else if (day==2)
+			return "Monday";
+		else if (day==3)
+			return "Tuesday";
+		else if (day==4)
+			return "Wensday";
+		else if (day==5)
+			return "Sunday";
+		else if (day==6)
+			return "Sunday";
+		else if (day==7)
+			return "Sunday";
+	}
+
+}
---
+++ Seminar_1/src/ro/ase/csie/stud/seminar2/Main.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.stud.seminar2;
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		CalendarUtil cu = 
+	}
+
+}
