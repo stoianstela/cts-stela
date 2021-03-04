d2eda90293a5fd09bf7a554d1e1b07350bb4eeca
stoia <stoia@LAPTOP-4DK1TJ8U>
2021-03-04 15:15:55 +0200

    seminar

--- Seminar_1/src/ro/ase/csie/stud/seminar2/Main.java
+++ Seminar_1/src/ro/ase/csie/stud/seminar2/Main.java
@@ -1,31 +1,45 @@
 package ro.ase.csie.stud.seminar2;
+
+import ro.ase.csie.stud.semina2.solid.BankAccount;
+import ro.ase.csie.stud.semina2.solid.Persoana;
+
 public class Main {
 	
 	public static void main(String[] args)
 	{
 		
-				CalendarUtil cu= new CalendarUtil();
-				
-				
-			
-				try {
-					System.out.println(cu.weekDay(10));
-					
-					System.out.println(cu.weekDay(3));
-					
-					System.out.println(cu.weekDay2(3));
-					
-					System.out.println(cu.weekDay3(3));
-					
-					System.out.println(cu.weekDay3(10));
-					
-					System.out.println("DateFormatSymbols says" + cu.weekDay4(10));
-					System.out.println("DateFormatSymbols says" + cu.weekDay4(5));
-				} catch (IncorrectDayException e) {
-					
-					e.printStackTrace();
-				}
+		
+		Persoana p = new Persoana("Stela");
+		BankAccount account = new BankAccount("INGB73463832",p);
+		
+//				CalendarUtil cu= new CalendarUtil();
+//				
+//				
+//			
+//				try {
+//					System.out.println(cu.weekDay(10));
+//					
+//					System.out.println(cu.weekDay(3));
+//					
+//					System.out.println(cu.weekDay2(3));
+//					
+//					System.out.println(cu.weekDay3(3));
+//					
+//					System.out.println(cu.weekDay3(10));
+//					
+//					System.out.println("DateFormatSymbols says" + cu.weekDay4(10));
+//					System.out.println("DateFormatSymbols says" + cu.weekDay4(5));
+//				} catch (IncorrectDayException e) {
+//					
+//					e.printStackTrace();
+//				}
 
+		
+		account.deposit(100);
+		account.withdraw(50);
+		account.withdraw(70);
+		
+		System.out.println(account.getBalance());
 		 
 	}
 
