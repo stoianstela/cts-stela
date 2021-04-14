6258f4f8d5b515fce07a090abe7f37f1ec6eaee3
stoia <stoia@LAPTOP-4DK1TJ8U>
2021-04-14 15:34:59 +0300

    Final

---
+++ Laborator 8/src/ro/ase/csie/cts/g1087/dp/adapter/AdapterDisneyLaACME.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1087.dp.adapter;
+
+public class AdapterDisneyLaACME {
+
+}
---
+++ Laborator 8/src/ro/ase/csie/cts/g1087/dp/decorator/TestDecorator.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1087.dp.decorator;
+
+public class TestDecorator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
