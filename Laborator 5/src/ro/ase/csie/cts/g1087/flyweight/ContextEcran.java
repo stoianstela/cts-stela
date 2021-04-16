085398264994c7e02cce6b3a3912e32900e3f0d2
stoia <stoia@LAPTOP-4DK1TJ8U>
2021-04-16 11:25:51 +0300

    Flyweight

--- Laborator 5/src/ro/ase/csie/cts/g1087/flyweight/ContextEcran.java
+++ Laborator 5/src/ro/ase/csie/cts/g1087/flyweight/ContextEcran.java
@@ -1,5 +1,35 @@
 package ro.ase.csie.cts.g1087.flyweight;
 
 public class ContextEcran {
+	
+	int x;
+	int y;
+	String culoareTextura;
+	public ContextEcran(int x, int y, String culoareTextura) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.culoareTextura = culoareTextura;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public String getCuloareTextura() {
+		return culoareTextura;
+	}
+	public void setCuloareTextura(String culoareTextura) {
+		this.culoareTextura = culoareTextura;
+	}
+	
+	
 
 }
--- Laborator 5/src/ro/ase/csie/cts/g1087/flyweight/InterfataModel3D.java
+++ Laborator 5/src/ro/ase/csie/cts/g1087/flyweight/InterfataModel3D.java
@@ -2,4 +2,6 @@ package ro.ase.csie.cts.g1087.flyweight;
 
 public interface InterfataModel3D {
 
+	public void incarcaModel3D(String fisier);
+	public void afisareEcran(ContextEcran context);
 }
