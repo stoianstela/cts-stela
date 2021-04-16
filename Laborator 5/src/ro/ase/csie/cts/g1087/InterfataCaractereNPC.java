39e22f0f0e581ff7e093b1a772b6725976337936
stoia <stoia@LAPTOP-4DK1TJ8U>
2021-04-16 10:34:16 +0300

    Implementare Composite

---
+++ Laborator 5/src/ro/ase/csie/cts/g1087/InterfataCaractereNPC.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1087;
+
+public interface InterfataCaractereNPC {
+	public void atacaJucator(String numeJucator);
+	public void seRetrage();
+	public void seDeplaseaza();
+	
+	
+}
---
+++ Laborator 5/src/ro/ase/csie/cts/g1087/TestComposite.java
@@ -0,0 +1,27 @@
+package ro.ase.csie.cts.g1087;
+
+public class TestComposite {
+
+	public static void main(String[] args) {
+		
+		
+	GrupCaractere grup1 = new GrupCaractere("Grup1");
+	grup1.adaugaNod(new CaracterNPC("Soldat 1",100));
+	grup1.adaugaNod(new CaracterNPC("Soldat 2",100));
+	
+	GrupCaractere grup2 = new GrupCaractere("Grup2");
+	grup2.adaugaNod(new CaracterNPC("Soldat 3",200));
+	grup2.adaugaNod(new CaracterNPC("Tanc",500));
+	
+	GrupCaractere grupNivelJoc = new GrupCaractere("Level2");
+	grupNivelJoc.adaugaNod(new CaracterNPC("Level Boss",1000));
+	grupNivelJoc.adaugaNod(grup1);
+	grupNivelJoc.adaugaNod(grup2);
+	
+	grupNivelJoc.atacaJucator("Superman");
+	grupNivelJoc.seRetrage();
+	grupNivelJoc.getNod(0).seDeplaseaza();
+	
+	}
+
+}
