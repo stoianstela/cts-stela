43feeeccf60b477b518eae5b63a07798bf0adc39
stoia <stoia@LAPTOP-4DK1TJ8U>
2021-05-19 15:29:34 +0300

    Testare Unitara

--- Laborator 12 - Testare unitara/src/ro/ase/csie/g1087/testare/modelele/OperatiiMatematice.java
+++ Laborator 12 - Testare unitara/src/ro/ase/csie/g1087/testare/modelele/OperatiiMatematice.java
@@ -14,6 +14,6 @@ public class OperatiiMatematice {
 	{
 		if(b == 0)
 			throw new ExceptieImpartireLaZero();
-		return a/b;
+		return (double)a/b;
 	}
 }
--- Laborator 12 - Testare unitara/src/ro/ase/csie/g1087/testare/teste/TestOperatiiMatematice.java
+++ Laborator 12 - Testare unitara/src/ro/ase/csie/g1087/testare/teste/TestOperatiiMatematice.java
@@ -8,24 +8,29 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ro.ase.csie.g1087.testare.exceptii.ExceptieImpartireLaZero;
 import ro.ase.csie.g1087.testare.modelele.OperatiiMatematice;
 
 public class TestOperatiiMatematice {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Salut");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		System.out.println("La revedere!");
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("Pregtire test");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("Stergere resurse folosite ");
 	}
 
 	@Test
@@ -44,6 +49,28 @@ public class TestOperatiiMatematice {
 		int rezultatCalculat = OperatiiMatematice.aduna(a, b);
 		
 		assertEquals("Suma cu numere pozitive", rezultatAstepta, rezultatCalculat);
+	}
+	
+	@Test
+	public void testImpartireValoriPozitive()
+	{
+		int a = 1;
+		int b = 2;
+		double rezultatAsteptat = 0.5;
+		double rezultatCalculat = 0;
+		try
+		{
+			rezultatCalculat = OperatiiMatematice.impartire(a, b);
+		}
+		catch(ExceptieImpartireLaZero e)
+		{
+			fail("Am primit excceptie desi nu trebuia");
+		}
+		
+		assertEquals("Impartire 1/2", rezultatAsteptat, rezultatCalculat,0);
+		
+		
+		
 	}
 
 }
